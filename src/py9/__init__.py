ca790008be6e96c829aa704fdf5dd6c8dc414002
Gareth Davidson <gaz@bitplane.net>
2025-08-22 18:40:34 +0100

    move to __init__.py

---
+++ src/py9/__init__.py
@@ -0,0 +1,9 @@
+"""
+Import from those modules directly or use the main package imports.
+"""
+
+from .key import Py9Key
+from .dict import Py9Dict
+from .input import Py9Input
+
+__all__ = ["Py9Key", "Py9Dict", "Py9Input"]
